a904975be8fb6a490d9b80a58c35358907b1cf8f
Emil <emil88pl@gmail.com>
2023-07-03 16:04:53 +0100

    Spring Security

---
+++ src/main/java/com/example/test/TestApplication.java
@@ -0,0 +1,23 @@
+// package com.example.test;
+
+// import org.springframework.boot.SpringApplication;
+// import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+// @SpringBootApplication
+// public class TestApplication { // Test
+
+// 	public static void main(String[] args) {
+// 		SpringApplication.run(TestApplication.class, args);
+// 	}
+
+// }
+
+// The short answer is:
+
+// Maven is build tool (like Make is for C/C++, or Ant and Gradle are for Java). 
+// It compiles your source code into binary, runs the tests etc.
+
+// Spring Boot is a library, which helps you create an application faster. 
+// It is basically a bunch of code written by others available for you to use it.
+
+// Your application can use Spring Boot as a library, and you can build it via Maven, so you can run your application as well.
